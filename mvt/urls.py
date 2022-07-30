0fcd3402351cc6905fc7dbbf419ab112415c0c24
Mateo.Gomez <gomezmaryncakmateo@gmail.com>
2022-07-30 18:13:07 -0300

    commit avanzado 3

--- mvt/urls.py
+++ mvt/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app1/",include("app1.urls")),
+    path("",include("app1.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #para las fotos
