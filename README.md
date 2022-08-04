32223177f62d475b7699b3587fc212e8d31944c2
Mateo.Gomez <gomezmaryncakmateo@gmail.com>
2022-08-04 01:40:29 -0300

    commit heroku

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[!Deploy](https://heroku.com/deploy?template=https://github.com/Mateeeee841/Proyecto-final)
 # Proyecto-final
 Proyecto final de Francisco Castro y Mateo Gomez Maryncak
--- mvt/settings.py
+++ mvt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-6mcgcwpnk@$014ur3&8-g68-ujt3-ly9-5=x*p$=ogzi02b$xg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST"), "localhost"]
 
 
 # Application definition
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,7 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mvt.wsgi.application'
 
-
+conn = os.environ.get("DATABASE_URL", 'sqlite:///db.sqlite3')
+DATABASES = {"default": dj_database_url.parse(conn, conn_max_age=600)}
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -115,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
